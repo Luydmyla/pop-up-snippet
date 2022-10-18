96eb18b78ddd7f8902401865827ad2733270e015
Luydmyla <lytic.ok@gmail.com>
2022-10-18 15:30:20 +0300

    added video

--- pop-up.js
+++ pop-up.js
@@ -1,6 +1,3 @@
-// import './sass/main.scss';
-// import './sass/_button-show-more.scss';
-
 (() => {
   const refs = {
     showMoreBtn: document.querySelector("[data-btn-show-open]"),
@@ -18,19 +15,19 @@
   function toggleActiveBtn() {
     refs.showMoreBtn.classList.toggle("active");
   }
-  // ---добавляю слушательсобытий клавиатуры на кнопку эскейп при открытом модальном окне-----------
+  // ---добавляю слухача подій клавіатури на кнопку эскейп при відкритому модальному вікні-----------
   document.addEventListener("keydown", onClick);
-  //   instance.element().querySelector('.btn-close').onclick = instance.close;
 
+  // ----------закрити вікно по кнопці Эскейп-------------
   function onClick(e) {
-    // ----------закрыла окно по кнопке Эскейп-------------
+    if (refs.showMoreModal.classList.contains("is-hidden")) {
+      return;
+    }
     if (e.code === "Escape") {
       console.log("закрываем");
       toggleShowMoreModal();
-      //   toggleActiveBtn();
-      document.removeEventListener("keydown", onClick);
+      // document.removeEventListener("keydown", onClick);
     }
   }
-
   //   document.removeEventListener('keydown', onClick);
 })();
